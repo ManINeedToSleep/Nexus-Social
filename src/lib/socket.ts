99bc3d8d9d05ca721fe7808a99aa63a0d43d046e
ManINeedToSleep <bguna0050@launchpadphilly.org>
2025-02-10 00:35:48 -0500

    added vercel stuff

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -9,7 +9,7 @@
 
 import { io } from "socket.io-client";
 
-// Create a single socket instance for the entire application
-const socket = io("http://localhost:3001");
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL || "http://localhost:3001";
+const socket = io(SOCKET_URL);
 
 export default socket; 
